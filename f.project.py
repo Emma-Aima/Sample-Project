7f578a906642dc103d4d3ddb992a396796a351de
Emmanuella <aimalohiileogben@gmail.com>
2025-07-14 03:01:59 -0700

    Update f.project.py
    New

--- f.project.py
+++ f.project.py
@@ -1,8 +1,13 @@
 # Cleaned and Corrected Version of Your Code
 
 # --------- First Cell: Simulate Health Data ---------
+import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from datetime import datetime, timedelta
 !pip install scikit-learn
 scikit-learn==1.4.2
 
